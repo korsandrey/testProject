a11df2ce69c673c734f0890888bd9e8a02d0543b
korsandrey <96125098+korsandrey@users.noreply.github.com>
2025-07-19 23:02:08 +0300

    Delete TestProject directory

--- TestProject/part1/LibraryDynHeader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-extern "C" __declspec(dllexport) void makeNewEnterense();
-extern "C" __declspec(dllexport) void writeSomeInformation(std::string , char );
-extern "C" __declspec(dllexport) void changeDefaultLevel();
-extern "C" __declspec(dllexport) std::string showTimeD();
-extern "C" __declspec(dllexport) void makeFName(std::string );
-extern "C" __declspec(dllexport) void makeILevel(char );
--- TestProject/part1/LibraryStatHeader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-void makeNewEnterense();
-void makeFName(std::string );
-void makeILevel(char);
-void writeSomeInformation(std::string , char );
-void changeDefaultLevel();
-std::string showTime();
