28efc47957d4730769f6c06ef73ea5f260890c73
korsandrey <96125098+korsandrey@users.noreply.github.com>
2025-07-23 22:45:54 +0300

    Add files via upload

---
+++ CMakeProject1/CMakeProject1/LibraryStatHeader.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void makeNewEnterense();
+void makeFName(std::string );
+void makeILevel(char);
+void writeSomeInformation(std::string , char );
+void changeDefaultLevel();
+std::string showTime();
